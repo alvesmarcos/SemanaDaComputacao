5da8f9d1c77da3b83401457a2cd183f02b0cba2c
Marcos Alves <alves.henrique.marcos@gmail.com>
2018-05-20 19:32:21 -0300

    Adiciona validacao de campos

--- src/util/index.js
+++ src/util/index.js
@@ -26,4 +26,49 @@ const transitionConfig = () => {
   }
 };
 
-export { transitionConfig };
+const validateEmail = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+// logica pode alterar
+const validateSenha = (senha) => {
+  try {
+    const len = senha.length;
+    if (len >= 8) {
+      return true; 
+    }
+  } catch (e) {
+    // console.log(e);
+  }
+  return false;
+};
+
+// logica pode alterar
+const validateNome = (nome) => {
+  try {
+    if (nome) {
+      return true; 
+    }
+  } catch (e) {
+    // console.log(e);
+  }
+  return false;
+};
+
+// logica pode alterar
+const validateCurso = (senha) => {
+  try {
+    const len = senha.length;
+    if (len >= 3) {
+      return true; 
+    }
+  } catch (e) {
+    // console.log(e);
+  }
+  return false;
+};
+
+
+
+export { transitionConfig, validateEmail, validateSenha, validateNome, validateCurso };
