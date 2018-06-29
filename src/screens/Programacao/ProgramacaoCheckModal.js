163bc49bcc61fdd6404d4114a32f8f3f9a3c9ae7
alvesmarcos <alves.henrique.marcos@gmail.com>
2018-06-28 21:10:31 -0300

    Adiciona check-in modal

--- src/screens/Programacao/ProgramacaoCheckModal.js
+++ src/screens/Programacao/ProgramacaoCheckModal.js
@@ -3,6 +3,7 @@ import {
   View,
   StatusBar,
   Text,
+  ScrollView,
 } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 import { colors } from '../../styles';
@@ -35,11 +36,68 @@ class ProgramacaoCheckModal extends React.Component {
             size={50}
             color={colors.white}
           />
-          <View style={{ marginLeft: 26  }}>
-            <Text style={{ color: colors.white, fontFamily: 'Lato-Regular', fontSize: 16, marginBottom: 4 }}>{'Horário de início desta atividade'}</Text>
+          <View style={{ marginLeft: 20  }}>
+            <Text style={{ color: colors.white, fontFamily: 'Lato-Regular', fontSize: 14, marginBottom: 4 }}>{'Horário de início desta atividade'}</Text>
             <Text style={{ color: colors.white, fontFamily: 'Lato-Regular', fontSize: 26, textAlign: 'center' }}>{'07h30min'}</Text>
           </View>
         </View>
+        <ScrollView>
+        <View style={{ flexDirection: 'column', elevation: 5, padding: 16, backgroundColor: '#fff', margin: 8, }}>
+          <View style={{ flexDirection: 'row' }}>
+            <Feather
+              name={'menu'}
+              size={20}
+              color={'gray'}
+            />
+            <Text style={{ fontFamily: 'Lato-Bold', fontSize: 16, marginLeft: 12 }}>{'Nome'}</Text>
+          </View>
+          <Text style={{ fontFamily: 'Lato-Regular', fontSize: 16, marginLeft: 32, marginTop: 4 }}>{'Check-in'}</Text>
+          <View style={{ borderTopWidth: 1, borderTopColor: '#eee', marginVertical: 16 }} />
+          
+          <View style={{ flexDirection: 'row' }}>
+            <Feather
+              name={'home'}
+              size={20}
+              color={'gray'}
+            />
+            <Text style={{ fontFamily: 'Lato-Bold', fontSize: 16, marginLeft: 12 }}>{'Local'}</Text>
+          </View>
+          <Text style={{ fontFamily: 'Lato-Regular', fontSize: 16, marginLeft: 32, marginTop: 4 }}>{'Hall de Entrada - Centro de Informática'}</Text>
+          <View style={{ borderTopWidth: 1, borderTopColor: '#eee', marginVertical: 16 }} />
+          
+          <View style={{ flexDirection: 'row' }}>
+            <Feather
+              name={'layers'}
+              size={20}
+              color={'gray'}
+            />
+            <Text style={{ fontFamily: 'Lato-Bold', fontSize: 16, marginLeft: 12 }}>{'Categoria'}</Text>
+          </View>
+          <Text style={{ fontFamily: 'Lato-Regular', fontSize: 16, marginLeft: 32, marginTop: 4 }}>{'Check-in & Inscrições'}</Text>
+          <View style={{ borderTopWidth: 1, borderTopColor: '#eee', marginVertical: 16 }} />
+          
+          <View style={{ flexDirection: 'row' }}>
+            <Feather
+              name={'clock'}
+              size={20}
+              color={'gray'}
+            />
+            <Text style={{ fontFamily: 'Lato-Bold', fontSize: 16, marginLeft: 12 }}>{'Término'}</Text>
+          </View>
+          <Text style={{ fontFamily: 'Lato-Regular', fontSize: 16, marginLeft: 32, marginTop: 4 }}>{'17h45min'}</Text>
+          <View style={{ borderTopWidth: 1, borderTopColor: '#eee', marginVertical: 16 }} />
+
+          <View style={{ flexDirection: 'row' }}>
+            <Feather
+              name={'alert-circle'}
+              size={20}
+              color={'gray'}
+            />
+            <Text style={{ fontFamily: 'Lato-Bold', fontSize: 16, marginLeft: 12 }}>{'Descrição'}</Text>
+          </View>
+          <Text style={{ fontFamily: 'Lato-Regular', fontSize: 16, marginLeft: 32, marginTop: 4 }}>{'Para fazer o credenciamente tenha o ingresso em mãos de forma digital ou impressa, caso você deseje evitar filas tenha o aplicativo instalado e procure um ponto para validar seu ingresso eletrônico'}</Text>
+        </View>
+        </ScrollView>
       </View>
     );
   }
